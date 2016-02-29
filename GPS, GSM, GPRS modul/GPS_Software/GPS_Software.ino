0ae45b091d2cb703cf2b4c5e232d438f0061202d
Tomas_Moravec <tomas.moravec@tul.czmkdir>
2016-02-29 11:01:51 +0100

    Vylepsena verze softwaru, opakovane akce pridany do metod, osetreno cteni textu (nektereho)

--- GPS, GSM, GPRS modul/GPS_Software/GPS_Software.ino
+++ GPS, GSM, GPRS modul/GPS_Software/GPS_Software.ino
@@ -1,113 +1,79 @@
-char inchar;
-void setup()
-{    
-  //Init the driver pins for GSM function
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  Serial.begin(9600);                                      //set the baud rate
-  digitalWrite(5,HIGH);                                     //Output GSM Timing 
-  delay(1500);
-  digitalWrite(5,LOW);  
-  digitalWrite(3,LOW);                                      //Enable the GSM mode
-  digitalWrite(4,HIGH);                                     //Disable the GPS mode
-  delay(2000);
-  delay(5000);                                              //call ready
-  delay(5000);
-  Serial.println("AT+CMGD=1,4");                           //Delete all SMS in box
+#include <SoftwareSerial.h>
+SoftwareSerial gps(6, 7);  // RX, TX: Arduino -> PC
+
+byte gsmDriverPin[3] = {3,4,5};  // Pins of GPS
+int networkConnect = 2000;  // In real case, highly recommended bigger amount of time
+int baudRate = 9600;  // Communication baud rate for GPS and PC
+int common = 500;  // Delay between commands
+int CtrlC = 26;  // ASCII code for Ctrl+C
+int led = 13;  // Pin of signalization LED
+String phone = "776006865";  // Default phone number
+String string = "";  // Empty string
+boolean lock = false;  // Car is not locked
+
+void setup(){     
+  for(int i = 0 ; i < 3; i++){pinMode(gsmDriverPin[i],OUTPUT);}
+  pinMode(led,OUTPUT);  // Setting of LED pin
+  
+  Serial.begin(baudRate);  // Connect with PC (Serial monitor)
+  gps.begin(baudRate);  // Connect with GPS (GPS/GPRS/GSM Module v3.0)
+  
+  digitalWrite(gsmDriverPin[2],HIGH);  // Reset GSM timer
+  delay(common);
+  digitalWrite(gsmDriverPin[2],LOW);  // Enable GSM timer
+  digitalWrite(gsmDriverPin[0],LOW);  // Enable GSM
+  digitalWrite(gsmDriverPin[1],HIGH);  // Disable GPS
+
+  delay(networkConnect);  // Time for connect into network
+
+  Serial.println("-------- Nove komunikacni okno --------");
+  Serial.println();
+  sendCommand("AT+CMGD=1,4");  // Delete all SMS
+  digitalWrite(led,HIGH);  // GPS ready sign
 }
  
-void loop()
-{  
-  if(Serial.available()>0)
-  {
-    inchar=Serial.read();
-    if(inchar=='T')
-    {
-      delay(10);
-      inchar=Serial.read(); 
-      if (inchar=='I')                                      //When the GSM module get the message, it will display the sign '+CMTI "SM", 1' in the serial port
-      {      
-        delay(10);
-        Serial.println("AT+CMGR=1");                       //When Arduino read the sign, send the "read" AT command to the module
-        delay(10);
-      }
+void loop(){
+  if(gps.available() > 0){
+    string = readGPS();  // Read incoming GPS data
+
+    // New SMS indication and content of SMS
+    if(string.equalsIgnoreCase("+CMTI: \"SM\",1")){
+      Serial.println("---- Nova SMS ----");
+      sendCommand("AT+CMGR=1");  // Show content of SMS
+      //sendCommand("AT+CMGD=1,4");  // Delete all SMS
     }
-    else if (inchar=='U')
-    {
-      delay(10);
-      inchar=Serial.read(); 
-      if (inchar=='N')                                     //Thw SMS("LH") was display in the Serial port, and Arduino has recognize it.
-      {
-        delay(10);
-        inchar=Serial.read(); 
-        if (inchar=='L')                                     //Thw SMS("LH") was display in the Serial port, and Arduino has recognize it.
-        {
-          delay(10);
-          inchar=Serial.read(); 
-          if (inchar=='O')                                     //Thw SMS("LH") was display in the Serial port, and Arduino has recognize it.
-          {
-            delay(10);
-            inchar=Serial.read(); 
-            if (inchar=='C')                                     //Thw SMS("LH") was display in the Serial port, and Arduino has recognize it.
-            {
-              delay(10);
-              inchar=Serial.read(); 
-              if (inchar=='K')                                     //Thw SMS("LH") was display in the Serial port, and Arduino has recognize it.
-              {
-                Serial.println("AT"); //Send AT command  
-                delay(2000);
-                Serial.println("AT");   
-                delay(2000);
-                //Send message
-                Serial.println("AT+CMGF=1");
-                delay(1000);
-                Serial.println("AT+CMGS=\"776006865\"");//Change the receiver phone number
-                delay(1000);
-                Serial.print("Unlocked");//the message you want to send
-                delay(1000);
-                Serial.write(26);
-                delay(50);        
-                Serial.println("AT+CMGD=1,4");                    //Delete all message
-                delay(500);
-              }
-            }
-          }
-        }
-      }
-    }
-    else if (inchar=='L')
-    {
-      delay(10);
-      inchar=Serial.read(); 
-      if (inchar=='O')                                     //Thw SMS("LH") was display in the Serial port, and Arduino has recognize it.
-      {
-        delay(10);
-        inchar=Serial.read(); 
-        if (inchar=='C')                                     //Thw SMS("LH") was display in the Serial port, and Arduino has recognize it.
-        {
-          delay(10);
-          inchar=Serial.read(); 
-          if (inchar=='K')                                     //Thw SMS("LH") was display in the Serial port, and Arduino has recognize it.
-          {
-            Serial.println("AT"); //Send AT command  
-            delay(2000);
-            Serial.println("AT");   
-            delay(2000);
-            //Send message
-            Serial.println("AT+CMGF=1");
-            delay(1000);
-            Serial.println("AT+CMGS=\"776006865\"");//Change the receiver phone number
-            delay(1000);
-            Serial.print("Locked");//the message you want to send
-            delay(1000);
-            Serial.write(26);
-            delay(50);        
-            Serial.println("AT+CMGD=1,4");                    //Delete all message
-            delay(500);
-          }
-        }
-      }
+
+    // Content of SMS
+    if(string.equalsIgnoreCase("DOD")){
+      
     }
   }
+}
+
+String readGPS(){
+  String data;
+  while (gps.available() > 0){  // Until are data in buffer
+    data += gps.readString();  // Read all data in buffer
+  }
+  Serial.println(data);  // Send data to PC
+  data.trim();  // Delete any starting and ending whitespaces
+  return data;
+}
+
+void sendCommand(String command){
+  delay(common);
+  //Serial.print("Arduino -> GPS: ");  // Send data to PC
+  //Serial.println(command);  // Send data to PC
+  gps.println(command);  // Send data to GPS
+  delay(common);
+}
+
+void sendSMS(String sms){
+  Serial.println("AT+CMGF=1");  // Text mode
+  delay(common);
+  Serial.print("AT+CMGS=\"776006865\"");  // Add phone number
+  delay(common);
+  Serial.print(sms);  // Add text of SMS
+  delay(common);
+  Serial.write(CtrlC);  // Send SMS
 }
--- GPS, GSM, GPRS modul/Ukázkové kódy/Testing/Testing.ino
+++ GPS, GSM, GPRS modul/Ukázkové kódy/Testing/Testing.ino
@@ -1,56 +1,79 @@
 #include <SoftwareSerial.h>
-SoftwareSerial gps(6, 7);  // RX, TX
+SoftwareSerial gps(6, 7);  // RX, TX: Arduino -> PC
 
 byte gsmDriverPin[3] = {3,4,5};  // Pins of GPS
-int ledpin = 13;  // Signalization LED
+int networkConnect = 2000;  // In real case, highly recommended bigger amount of time
+int baudRate = 9600;  // Communication baud rate for GPS and PC
+int common = 500;  // Delay between commands
+int CtrlC = 26;  // ASCII code for Ctrl+C
+int led = 13;  // Pin of signalization LED
+String phone = "776006865";  // Default phone number
 String string = "";  // Empty string
 boolean lock = false;  // Car is not locked
-String phone = "+420776006865"  // Default phone number
 
 void setup(){     
   for(int i = 0 ; i < 3; i++){pinMode(gsmDriverPin[i],OUTPUT);}
-  pinMode(ledpin,OUTPUT);  // Setting of LED pin
+  pinMode(led,OUTPUT);  // Setting of LED pin
   
-  Serial.begin(9600);  // Connect with PC (Serial monitor)
-  gps.begin(9600);  // Connect with GPS (GPS/GPRS/GSM Module v3.0)
+  Serial.begin(baudRate);  // Connect with PC (Serial monitor)
+  gps.begin(baudRate);  // Connect with GPS (GPS/GPRS/GSM Module v3.0)
   
-  digitalWrite(5,HIGH);  // Reset GSM timer
-  delay(1500);
-  digitalWrite(5,LOW);  // Enable GSM timer
-  digitalWrite(3,LOW);  // Enable GSM
-  digitalWrite(4,HIGH);  // Disable GPS
-  delay(5000);
-  delay(5000);
-  delay(5000);
+  digitalWrite(gsmDriverPin[2],HIGH);  // Reset GSM timer
+  delay(common);
+  digitalWrite(gsmDriverPin[2],LOW);  // Enable GSM timer
+  digitalWrite(gsmDriverPin[0],LOW);  // Enable GSM
+  digitalWrite(gsmDriverPin[1],HIGH);  // Disable GPS
 
+  delay(networkConnect);  // Time for connect into network
+
+  Serial.println("-------- Nove komunikacni okno --------");
+  Serial.println();
   sendCommand("AT+CMGD=1,4");  // Delete all SMS
-  digitalWrite(ledpin,HIGH);  // GPS ready sign
+  digitalWrite(led,HIGH);  // GPS ready sign
 }
  
 void loop(){
   if(gps.available() > 0){
-    string = "";  // Clear string
-    while (gps.available() > 0) {
-      string += gps.readString();
-    }
-    Serial.println(string);  // Send data to PC
-    string.trim();  // Delete any starting and ending whitespaces
+    string = readGPS();  // Read incoming GPS data
+
+    // New SMS indication and content of SMS
     if(string.equalsIgnoreCase("+CMTI: \"SM\",1")){
-      Serial.println("NEW SMS");
+      Serial.println("---- Nova SMS ----");
+      sendCommand("AT+CMGR=1");  // Show content of SMS
+      //sendCommand("AT+CMGD=1,4");  // Delete all SMS
+    }
+
+    // Content of SMS
+    if(string.equalsIgnoreCase("DOD")){
+      
     }
   }
 }
 
+String readGPS(){
+  String data;
+  while (gps.available() > 0){  // Until are data in buffer
+    data += gps.readString();  // Read all data in buffer
+  }
+  Serial.println(data);  // Send data to PC
+  data.trim();  // Delete any starting and ending whitespaces
+  return data;
+}
+
 void sendCommand(String command){
-  delay(500);
-  Serial.print("Arduino -> GPS: ");
-  Serial.println(command);  // Send data to PC
+  delay(common);
+  //Serial.print("Arduino -> GPS: ");  // Send data to PC
+  //Serial.println(command);  // Send data to PC
   gps.println(command);  // Send data to GPS
-  delay(500);
+  delay(common);
 }
 
 void sendSMS(String sms){
-  
+  Serial.println("AT+CMGF=1");  // Text mode
+  delay(common);
+  Serial.print("AT+CMGS=\"776006865\"");  // Add phone number
+  delay(common);
+  Serial.print(sms);  // Add text of SMS
+  delay(common);
+  Serial.write(CtrlC);  // Send SMS
 }
-}
-
