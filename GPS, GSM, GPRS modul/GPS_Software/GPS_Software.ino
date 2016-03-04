c89b73c1a50e36216be2b61d4a20b833af19e813
Tomas_Moravec <tomas.moravec@tul.czmkdir>
2016-03-04 19:52:12 +0100

    Funkcni odpovedi na SMS

--- GPS, GSM, GPRS modul/GPS_Software/GPS_Software.ino
+++ GPS, GSM, GPRS modul/GPS_Software/GPS_Software.ino
@@ -8,11 +8,11 @@ int common = 500;  // Delay between commands
 int CtrlC = 26;  // ASCII code for Ctrl+C
 int led = 13;  // Pin of signalization LED
 char character = ' ';  // Empty character
-String phone = "776006865";  // Default phone number
+String ownerNumber = "776006865";  // Default phone number
 String string = "";  // Empty string
 String SMS = "";  // Empty SMS content
 boolean lock = false;  // Car is not locked
-boolean readSMS = false;  // Can i read test of SMS now?
+boolean readSMS = false;  // Can i read content of SMS now?
 
 void setup(){   
   // Setup shield  
@@ -46,28 +46,14 @@ void setup(){
 void loop(){
   // Communication: Shield -> Arduino
   if(gps.available()){
-    character = gps.read();  // Read one character from shield serial
-    string += character;  // Add character into string
-    Serial.write(character);  // Send data to PC
-
-    // If recieved command is completed
-    if(character == '\n'){
-      //Serial.println("Newline");
+    readGPS();
+    
+    if(character == '\n'){  // If recieved command is completed
       string.trim();  // Delete all whitespace characters
-
-      // New SMS indication and content of SMS
-      if(string.equalsIgnoreCase("+CMTI: \"SM\",1")){
-        sendCommand("AT+CMGR=1");  // Show content of SMS
-      }
-      else if(string.startsWith("+CMGR:")){
-        readSMS = true;
-      }
-      else if(readSMS == true){
-        Serial.print("Content of SMS: ");
-        Serial.println(string);
-        SMS = string;
-        readSMS = false;
-      }
+      
+      recognize_SMS_New();
+      recognize_SMS_Header();
+      recognize_SMS_Content();
       
       string = "";  // Delete content of string
     }
@@ -89,16 +75,46 @@ void loop(){
   }
 }
 
+void recognize_SMS_New(){
+  if(string.equalsIgnoreCase("+CMTI: \"SM\",1")){  // If SMS indication
+    sendCommand("AT+CMGR=1");  // Show content of SMS
+  }
+}
+
+void recognize_SMS_Header(){
+  if(string.startsWith("+CMGR:")){  // If SMS header
+    readSMS = true;  // Set: Content is ready to read
+    string = "";  // Delete content of string
+  }
+}
+
+void recognize_SMS_Content(){
+  if(string != "" && readSMS == true){  // If content is ready to read
+    SMS = string;  // Move content into another string
+    readSMS = false;  // Set: Content is not ready to read
+    string = "";  // Delete content of string
+    sendCommand("AT+CMGD=1,4");  // Delete all SMS
+    
+    sendSMS("Vase SMS: " + SMS);
+  }
+}
+
+void readGPS(){
+  character = gps.read();  // Read one character from shield serial
+  string += character;  // Add character into string
+  Serial.write(character);  // Send data to PC
+}
+
 void sendCommand(String command){
   gps.println(command);  // Send data to GPS
 }
 
-void sendSMS(String sms){
-  Serial.println("AT+CMGF=1");  // Text mode
-  delay(common);
-  Serial.print("AT+CMGS=\"776006865\"");  // Add phone number
-  delay(common);
-  Serial.print(sms);  // Add text of SMS
-  delay(common);
-  Serial.write(CtrlC);  // Send SMS
+void sendSMS(String text){
+  gps.println("AT+CMGF=1");  // Text mode
+  delay(common);  // Time for respond
+  gps.println("AT+CMGS=\"" + ownerNumber + "\"");  // Add phone number
+  delay(common);  // Time for respond
+  gps.print(text);  // Add text of SMS
+  delay(common);  // Time for respond
+  gps.write(CtrlC);  // Send SMS
 }
